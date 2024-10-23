cd40c37f36ef15177a8175410345e7a0c05c11b3
Michael Kirk <michael.code@endoftheworl.de>
2024-10-23 14:00:29 -0700

    use conventional geo formats

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
+use std::io::Write;
+use std::str::FromStr;
 use geo::{BooleanOps, MultiPolygon};
 
 fn main() {
-    let base = include_str!("../base.json");
-    let union = include_str!("../union_this.json");
+    let base = include_str!("../base.geojson");
+    let union = include_str!("../union.geojson");
 
-    let base: MultiPolygon = serde_json::from_str(&base).unwrap();
-    let union: MultiPolygon = serde_json::from_str(&union).unwrap();
+    let base: MultiPolygon = geojson::Geometry::from_str(base).expect("valid GeoJSON").try_into().expect("valid MultiPolygon");
+    let union: MultiPolygon = geojson::Geometry::from_str(union).expect("valid GeoJSON").try_into().expect("valid MultiPolygon");
 
     base.union(&union);
 
     println!("Hello, world!");
+}
+
+fn write_geojson_to_file(geometry: &MultiPolygon, path: &str) {
+    let converted_to_geojson = geojson::Value::from(geometry);
+    let file = std::fs::File::create(path).unwrap();
+    let mut writer = std::io::BufWriter::new(file);
+    writer.write_all(&converted_to_geojson.to_string().into_bytes()).unwrap();
 }
